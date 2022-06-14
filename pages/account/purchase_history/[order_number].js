d3861cc0a401649929fd8aa3f3c8219953bf9829
Steffanie <47458499+steffanie07@users.noreply.github.com>
2022-06-14 06:59:45 +0100

    add list of products

--- pages/account/purchase_history/[order_number].js
+++ pages/account/purchase_history/[order_number].js
@@ -9,8 +9,13 @@ import {
   TableRow,
   Tabs,
   TableBody,
+  TableFooter,
+  TablePagination,
   Button,
   Chip,
+  Grid,Divider,
+  Paper,
+  boxDefault
 } from "@mui/material";
 import moment from "moment";
 import { useEffect, useState } from "react";
@@ -18,71 +23,117 @@ import { connect } from "react-redux";
 import { Content, Title } from "../../../components/elements";
 import AccountLayout from "../../../components/Layouts/account";
 import { orderList } from "../../../connect/order";
+import { useRouter } from "next/router"
 import { priceFormatDecimal } from "../../../util/priceFormatter";
+import { jsPDF } from "jspdf";
+
+
 
 function PurchaseHistory(props) {
   useEffect(() => {
     props.getOrders();
   }, []);
+  const router = useRouter()
+  const { num } = router.query
+    const doc = new jsPDF();
+    const subTotal = props.transactions.results.reduce((acc, product) => {
 
-  return (
-    <AccountLayout>
-      <Title variant="h4" sx={{ paddingTop: 1, paddingBottom: 1 }}>
-        Purchase History Detail
-      </Title>
-      <Content>Let’s get you started with DrugStoc</Content>
-      <Box sx={{ marginTop: 5 }}>
-      <TableContainer>
-      <Table sx={{ minWidth: 700 }} aria-label="spanning table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Desc</TableCell>
-            <TableCell align="right">Qty.</TableCell>
-            <TableCell align="right">Sum</TableCell>
-          </TableRow>
-        </TableHead>
-        {/* <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.desc}>
-              <TableCell>{row.desc}</TableCell>
-              <TableCell align="right">{row.qty}</TableCell>
-              <TableCell align="right">{row.unit}</TableCell>
-              <TableCell align="right">{ccyFormat(row.price)}</TableCell>
-            </TableRow>
-          ))}
+           return product.total_amount;
 
-          <TableRow>
-            <TableCell rowSpan={3} />
-            <TableCell colSpan={2}>Subtotal</TableCell>
-            <TableCell align="right">{ccyFormat(invoiceSubtotal)}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Tax</TableCell>
-            <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
-            <TableCell align="right">{ccyFormat(invoiceTaxes)}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell colSpan={2}>Total</TableCell>
-            <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
-          </TableRow>
-        </TableBody> */}
-      </Table>
-    </TableContainer>
-      </Box>
-    </AccountLayout>
-  );
+    }, 0);
+
+    let accountLayout = <><AccountLayout>
+        <Title variant="h4" sx={{paddingTop: 1, paddingBottom: 1}}>
+            Purchase History Detail
+        </Title>
+        <Content>Let’s get you started with DrugStoc</Content>
+
+        <Grid container justify="right" sx={{paddingTop: 0, paddingBottom: 0,paddingLeft: 80}}>
+            <Button variant="contained" type="submit">Print Invoice</Button>
+        </Grid>
+        <Box sx={{marginTop: 5}}>
+            <TableContainer >
+            {props.transactions.results.map((rows, index) => (
+                <Box><Content>{rows.shipping_address.first_name }
+                      {rows.shipping_address.last_name},</Content>
+                <Content >{rows.shipping_address.address_line1},</Content>
+                <Content>{rows.shipping_address.address_line2},</Content>
+                <Content>{rows.shipping_address.region},</Content>
+                <Content>{rows.shipping_address.phone_number1}</Content>
+                </Box>
+            )) }
+
+                <Table sx={{minWidth: 700}} aria-label="spanning table">
+                    <TableHead>
+                        <TableRow>
+
+                            <TableCell>Product Name</TableCell>
+                            <TableCell>Product Desc</TableCell>
+                            <TableCell align="right">Qty.</TableCell>
+                            <TableCell align="right">Cost</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+
+                        {props.transactions.results.map((rows, index) => {
+                            if(rows.id == num) {
+
+                                return (
+
+                                rows.items.map((row) => (
+
+                                        <TableRow key={row.desc}>
+                                            <TableCell>{row.product.name}</TableCell>
+                                            <TableCell>{row.product.desc}</TableCell>
+                                            <TableCell>{row.quantity}</TableCell>
+                                            <TableCell>{priceFormatDecimal(row.unit_price * row.quantity)}</TableCell>
+                                        </TableRow>
+
+
+                                    )));
+
+                            }
+                            }
+                        )}
+                        <TableRow>
+                            <TableCell rowSpan={3} />
+                            <TableCell colSpan={2}>Subtotal</TableCell>
+                            <TableCell align="right">{priceFormatDecimal(subTotal)}</TableCell>
+                        </TableRow>,
+                        <TableRow>
+                            <TableCell>Tax</TableCell>
+                            <TableCell align="right"></TableCell>
+                            <TableCell align="right"></TableCell>
+                        </TableRow>,
+                        <TableRow>
+                            <TableCell>Total</TableCell>
+                            <TableCell align="right"></TableCell>
+                            <TableCell align="right">{priceFormatDecimal(subTotal)}</TableCell>
+                        </TableRow>,
+
+                    </TableBody>
+
+
+                </Table>
+            </TableContainer>
+        </Box>
+    </AccountLayout></>;
+    return accountLayout;
+
 }
 
+
 const mapStateToProps = (state) => {
-  return {
-    transactions: state.orders.orders,
-  };
-};
+      return {
+        transactions: state.orders.orders,
+      };
+    };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getOrders: () => dispatch(orderList()),
-  };
-};
+    const mapDispatchToProps = (dispatch) => {
+      return {
+        getOrders: () => dispatch(orderList()),
+      };
+    };
 
-export default connect(mapStateToProps, mapDispatchToProps)(PurchaseHistory);
+    export default connect(mapStateToProps, mapDispatchToProps)(PurchaseHistory);
