6814301328a33401de3d6c472ec43109c524b837
Steffanie <47458499+steffanie07@users.noreply.github.com>
2022-06-19 20:25:23 +0100

    UI update

--- components/Layouts/auth.js
+++ components/Layouts/auth.js
@@ -7,8 +7,11 @@ function AuthenticationsLayout({ children }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid item xs={6} sx={{display: { xs: "none", md: "flex" },background: "#f2f6fb !important", paddingTop: "56px !important",}}>
-          <AuthSideBar />
+        <Grid      component="img"
+                                  xs={6} sx={{display: { xs: "none", md: "flex" },background: "#f2f6fb !important",
+         }}   src="/forgot.png"
+        >
+
         </Grid>
         <Grid item xs={12} sm={12} md={6} sx={{  display: { md: "block" }, paddingTop: "56px !important", background: "#ffffff !important", height: "100vh", position: "relative", width: "100%" }}>
           <Container fixed sx={{  padding: { xs: "16px !important", md: "10px 70px 30px 30px !important",}, }}>
